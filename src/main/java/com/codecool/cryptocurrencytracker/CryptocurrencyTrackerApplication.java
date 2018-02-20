3ce06c1658ccf0913060b242b13674a7ee2890d6
feshout <feshout@gmail.com>
2018-02-20 13:04:20 +0100

    Initial commit

---
+++ src/main/java/com/codecool/cryptocurrencytracker/CryptocurrencyTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.cryptocurrencytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptocurrencyTrackerApplication.class, args);
+	}
+}
