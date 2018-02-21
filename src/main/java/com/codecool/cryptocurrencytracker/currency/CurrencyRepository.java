fe88b6651903aff8e9c88dab81e5ad148714a80c
feshout <maciej.ukowski@gmail.com>
2018-02-21 14:51:23 +0100

    Add CurrencyRespository to cooperate with db

--- src/main/java/com/codecool/cryptocurrencytracker/currency/CurrencyRepository.java
+++ src/main/java/com/codecool/cryptocurrencytracker/currency/CurrencyRepository.java
@@ -1,4 +1,10 @@
 package com.codecool.cryptocurrencytracker.currency;
 
-public interface CurrencyRespository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CurrencyRepository extends CrudRepository<Currency, Integer> {
+
+
 }
