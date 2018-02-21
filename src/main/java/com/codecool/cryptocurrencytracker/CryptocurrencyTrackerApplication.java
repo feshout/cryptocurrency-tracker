a2f271cfa61529bf2216b19b2a7830ccbccf2580
feshout <maciej.ukowski@gmail.com>
2018-02-21 14:50:27 +0100

    Add DataAcces to main method

--- src/main/java/com/codecool/cryptocurrencytracker/CryptocurrencyTrackerApplication.java
+++ src/main/java/com/codecool/cryptocurrencytracker/CryptocurrencyTrackerApplication.java
@@ -1,12 +1,16 @@
 package com.codecool.cryptocurrencytracker;
 
-import org.springframework.boot.SpringApplication;
+
+import com.codecool.cryptocurrencytracker.services.DataAccess;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CryptocurrencyTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CryptocurrencyTrackerApplication.class, args);
+    public static void main(String[] args) {
+
+	    DataAccess dataAccess = new DataAccess();
+
+
 	}
 }
